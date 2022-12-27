6a1ad1ad7241b043032a7642bb7fe46608fe6079
metanart <ilya.katassonov@gmail.com>
2022-12-27 10:42:23 +0000

    Added ThreeJS

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from 'components/app/app';
 
-import 'scss/static/reset.css';
-import 'utils/init-game';
+import 'global/scss/static/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
