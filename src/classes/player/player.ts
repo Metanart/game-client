ad9da83d5003ccf7848df518e18a7bc0604d9b22
metanart <ilya.katassonov@gmail.com>
2022-12-28 22:27:07 +0000

    Added simple object moving

--- src/classes/player/player.ts
+++ src/classes/player/player.ts
@@ -1,9 +1,13 @@
 import { BoxGeometry, Mesh, MeshPhongMaterial } from 'three';
+import { code } from 'three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements';
 
 import { Length } from 'global/measurements';
 
 export class Player extends Mesh {
     constructor() {
+        var xSpeed = 1;
+        var zSpeed = 1;
+
         const boxWidth = Length.Decimeter * 4;
         const boxHeight = Length.Meter * 1.5;
         const boxDepth = Length.Decimeter * 4;
@@ -11,6 +15,29 @@ export class Player extends Mesh {
 
         const material = new MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue
         super(geometry, material);
+
+        const onDocumentKeyDown = (event: KeyboardEvent) => {
+            var keyCode = event.code;
+            console.log('[IKATASONOV]:', keyCode);
+
+            if (keyCode == 'KeyW') {
+                this.position.x -= xSpeed;
+            } else if (keyCode == 'KeyS') {
+                this.position.x += xSpeed;
+            }
+
+            if (keyCode == 'KeyA') {
+                this.position.z += zSpeed;
+            } else if (keyCode == 'KeyD') {
+                this.position.z -= zSpeed;
+            }
+
+            if (keyCode == 'Escape') {
+                this.position.set(0, boxHeight / 2, 0);
+            }
+        };
+
+        document.addEventListener('keydown', onDocumentKeyDown, false);
 
         this.position.setY(boxHeight / 2);
     }
