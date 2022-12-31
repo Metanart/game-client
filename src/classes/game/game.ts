27d0ff85bf43ab8903a116176bc8ece101f9fb65
Metanart <ilya.katassonov@gmail.com>
2022-12-31 19:31:15 +0000

    Added simple floor and cannon debugger (#21)

--- src/classes/game/game.ts
+++ src/classes/game/game.ts
@@ -6,32 +6,49 @@ import { renderer } from 'classes/renderer/utils';
 
 import { MainScene } from 'scenes/main-scene';
 
-import { IS_DEVELOPMENT_MODE } from 'constants/mode';
-
 import { gridHelper } from 'utils/get-development-grid';
 import { initDevelopmentCamera } from 'utils/init-development-camera';
+
+import { Body, Plane, World } from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
+
 const clock = new Clock();
 
 export class Game {
+    private world = new World();
     private camera = new Camera();
     private scene = new MainScene();
     // @ts-ignore
     private stats: Stats = new Stats();
+    private cannonDebugger;
 
     constructor(parentElement: HTMLElement) {
-        if (IS_DEVELOPMENT_MODE) {
-            initDevelopmentCamera(this.camera);
-            this.scene.add(gridHelper);
-        }
+        initDevelopmentCamera(this.camera);
+        this.scene.add(gridHelper);
+        this.setupStats();
+        this.setupPhysics();
+
+        // @ts-ignore
+        this.cannonDebugger = new CannonDebugger(this.scene, this.world, {});
 
         parentElement.appendChild(renderer.domElement);
 
-        this.setupStats();
         this.tick();
     }
 
     render() {
         renderer.render(this.scene, this.camera);
+    }
+
+    setupPhysics() {
+        this.world.gravity.set(0, -10, 0);
+
+        // Floor
+        const floorShape = new Plane();
+        const floorBody = new Body({ mass: 0 });
+        floorBody.addShape(floorShape);
+        floorBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+        this.world.addBody(floorBody);
     }
 
     setupStats() {
@@ -40,8 +57,12 @@ export class Game {
 
     tick() {
         requestAnimationFrame(this.tick.bind(this));
+
         this.stats.update();
-        this.render();
+
         this.scene.tick(clock.getDelta());
+        this.cannonDebugger.update();
+        this.render();
+        this.world.fixedStep();
     }
 }
--- src/classes/workbench/workbench.ts
+++ src/classes/workbench/workbench.ts
@@ -4,7 +4,11 @@ import { Colors } from 'tokens/colors';
 
 import { workbenchConfig } from './config';
 
+import { Body, Box, Plane, Vec3, World } from 'cannon-es';
+
 export class Workbench extends Mesh {
+    private body: Body;
+
     constructor() {
         const { width, height, depth } = workbenchConfig.dimensions;
         const geometry = new BoxGeometry(width, height, depth);
@@ -13,5 +17,15 @@ export class Workbench extends Mesh {
         super(geometry, material);
 
         this.position.setY(height / 2);
+
+        // Cube body
+        const shape = new Box(new Vec3(0.5, 0.5, 0.5));
+        this.body = new Body({ mass: 5 });
+        this.body.addShape(shape);
+        this.body.position.set(0, 5, 0);
+    }
+
+    getBody() {
+        return this.body;
     }
 }
