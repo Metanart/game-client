69e9998b30026924afc726e4e85df455f2edd4d5
Metanart <ilya.katassonov@gmail.com>
2023-01-01 19:21:27 +0000

    Updated camera and packages, removed depricated replacer

--- src/components/game/game-devtools.tsx
+++ src/components/game/game-devtools.tsx
@@ -3,13 +3,17 @@ import { FC, Fragment } from 'react';
 import { OrbitControls, Stats } from '@react-three/drei';
 
 import { Colors } from 'tokens/colors';
+import { Length } from 'tokens/measurements';
 
 export const GameDevtools: FC = () => {
     return (
         <Fragment>
             <Stats />
             <OrbitControls />
-            <gridHelper args={[100, 100, Colors.Red, 'teal']} position={[0.5, 0, 0.5]} />
+            <gridHelper
+                args={[Length.Hectometer, Length.Hectometer, Colors.Red, 'teal']}
+                position={[Length.Meter / 2, 0, Length.Meter / 2]}
+            />
         </Fragment>
     );
 };
--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -2,4 +2,14 @@ import { FC, ReactNode } from 'react';
 
 import { Canvas } from '@react-three/fiber';
 
-export const Game: FC<{ children?: ReactNode }> = (props) => <Canvas>{props.children}</Canvas>;
+import { Vector3 } from 'three/src/Three';
+
+const cameraConfig = {
+    fov: 50,
+    aspect: window.innerWidth / window.innerHeight,
+    near: 1,
+    far: 300,
+    position: new Vector3(-30, 30, 0),
+};
+
+export const Game: FC<{ children?: ReactNode }> = (props) => <Canvas camera={cameraConfig}>{props.children}</Canvas>;
