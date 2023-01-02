5ce5b2a2226bf16f221471a93d422195d7b42c53
metanart <ilya.katassonov@gmail.com>
2023-01-02 22:39:18 +0000

    Added movement

--- src/components/spine/use-spine-asset.ts
+++ src/components/spine/use-spine-asset.ts
@@ -1,7 +1,4 @@
-import { Euler } from 'three';
-
 import { SkeletonMesh } from '@esotericsoftware/spine-threejs';
-import { AxisRotator } from '@react-three/drei/web/pivotControls/AxisRotator';
 
 import { ESpines } from 'classes/spine/enums';
 import { SpineAssetsManager } from 'classes/spine/spine-assets-manager';
@@ -10,9 +7,10 @@ export const useSpineAsset = async (spineKey: ESpines) => {
     const assetManager = new SpineAssetsManager(ESpines.RAPTOR);
     await assetManager.load(spineKey);
     const skeletonMesh = assetManager.getSkeletonMesh() as SkeletonMesh;
-    //skeletonMesh.rotateZ(Math.PI / 4);
+
     skeletonMesh.rotateX(-Math.PI / 8);
-    //skeletonMesh.rotateY(Math.PI / 4);
+    skeletonMesh.position.z = -1;
+    skeletonMesh.scale.x = -1;
 
     return {
         skeletonMesh,
